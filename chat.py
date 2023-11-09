9fb3951861b4c1ecb70dabbebf054b2f4290e3b1
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2023-11-09 15:44:13 +0700

    feat: create chat message constant

---
+++ chat.py
@@ -0,0 +1,20 @@
+START = '''<b>Selamat datang di chatbot TI UINWS</b> 🤙🤙
+
+Kamu dapat mencari informasi seputar Prodi Teknologi Informasi UIN Walisongo Semarang.
+
+Informasi yang dapat kamu dapatkan meliputi:
+- 
+- 
+- 
+- 
+'''
+
+THANKS_FOR_FEEDBACK = "Terimakasih, semoga feedback yang anda berikan dapat membuat chatbot lebih baik lagi kedepanya :)"
+
+REQUEST_QUESTION = "Tolong masukkan request pertanyaan yang akan ditambahkan ke dalam chatbot"
+
+CANCEL_REQUEST_QUESTION = "Proses penambahan data telah ditabalkan :)"
+
+REQUEST_QUESTION_RESULT = "Pertanyaan yang sudah anda masukkan kedalam database adalah"
+
+ERROR_ADD_QUESTION = "Terjadi kesalahan, mohon ulangi lagi :)"
