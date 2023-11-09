b3fb2dfaa02ec10fdbb029c13e30ffa25b2f4e08
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2023-11-09 15:46:40 +0700

    feat: adding new handler for add questions

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import chat
+import config
+
+import requests
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, ConversationHandler, filters
@@ -7,34 +11,43 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-# conversation mode
+#* conversation mode
 FIRST_STATE = range(1)
-
 async def start_add_question_mode(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Tolong masukkan request pertanyaan yang akan ditambahkan ke dalam chatbot")
+    await update.message.reply_text(chat.REQUEST_QUESTION)
     return FIRST_STATE
 
 async def add_question(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_response = update.message.text
-    await update.message.reply_text(f"Pertanyaan yang sudah anda masukkan kedalam database adalah \n\n{user_response}")
-    await update.message.reply_text("Terimakasih semoga feedback yang anda berikan dapat membuat chatbot lebih baik lagi kedepanya :)")
+
+    req_data = {
+        'data': {
+            'question': user_response
+        }
+    }
+    res = requests.post(f'{config.SERVER_URL}/questions', json=req_data, headers=config.HEADERS)
+    if res.status_code != 200:
+        await update.message.reply_text(chat.ERROR_ADD_QUESTION)
+    else:
+        await update.message.reply_html(f"{chat.REQUEST_QUESTION_RESULT} \n\n<b>{user_response}</b>")
+        await update.message.reply_text(chat.THANKS_FOR_FEEDBACK)
     return ConversationHandler.END
 
 async def cancel_add_question_mode(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Proses dibatalkan")
+    await update.message.reply_text(chat.CANCEL_REQUEST_QUESTION)
     return ConversationHandler.END
 
 
-# default mode
+#* default mode
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print("last name sender", update.message.chat.last_name)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hai sayang :)")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=chat.START, parse_mode='HTML')
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="", parse_mode='HTML')
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('6320237733:AAG_xry4Hsj9lhCXZ4gazEa_wt_8UzCrN_s').build()
+    application = ApplicationBuilder().token(config.CHATBOT_TOKEN).build()
     
     # conversation handler
     add_mode_handler = ConversationHandler(
@@ -42,14 +55,14 @@ if __name__ == '__main__':
         states={
             FIRST_STATE: [MessageHandler(filters.TEXT & (~filters.COMMAND), add_question)],
         },
-        fallbacks=[CommandHandler("cancel_add", cancel_add_question_mode)]
+        fallbacks=[CommandHandler("cancel", cancel_add_question_mode)]
     )
     
-    # handler
+    #* handler
     start_handler = CommandHandler('start', start)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
 
-    # register handler
+    #* register handler
     application.add_handler(start_handler)
     application.add_handler(add_mode_handler)
     application.add_handler(echo_handler)
